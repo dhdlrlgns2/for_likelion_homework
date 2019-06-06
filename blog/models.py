01229c559b56da5eff73e45ba270b70015402885
dhdlrlgns2 <44688705+dhdlrlgns2@users.noreply.github.com>
2019-06-06 21:13:03 +0900

    Add files via upload
    댓글기능 추가

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length = 200)
@@ -11,3 +11,11 @@ class Blog(models.Model):
 
     def summary(self):
         return self.body[:100]
+
+class Comment(models.Model):
+    post = models.ForeignKey(Blog,on_delete=models.CASCADE,related_name="comments")
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    body = models.CharField(max_length = 500)
+
+    def __str__(self):
+        return str(self.post) + " " + str(self.user) + " " + str(self.body[:20])
